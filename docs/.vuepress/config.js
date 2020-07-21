65afb59cefa356748553ac48d65fbe7a4800cf78
王玉鹏 <550902023@qq.com>
2020-07-21 13:13:06 +0800

    添加统计代码

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,7 +3,20 @@ module.exports = {
     description: '欢迎来到我的博客',
     head: [
         ['link', {rel: 'icon', href: 'http://cdn.triumphxx.com.cn/img/%E5%85%AC%E4%BC%97%E5%8F%B71.jpeg'}],
+        [
+            "script",
+            {}, `
+            var _hmt = _hmt || [];
+            (function() {
+                var hm = document.createElement("script");
+                hm.src = "https://hm.baidu.com/hm.js?3dc907d6a99cfe1f7450b2092ff744e1";
+                var s = document.getElementsByTagName("script")[0];
+                s.parentNode.insertBefore(hm, s);
+            })();
+      `
+        ]
     ],
+
     themeConfig: {
         logo: 'http://cdn.triumphxx.com.cn/img/%E5%85%AC%E4%BC%97%E5%8F%B71.jpeg',
         nav: [
